f6bf9f88cc9cafe8d50adb7041f727acdf108ec0
Kamila Rocha <krp@discente.ifpe.edu.br>
2024-03-18 10:28:36 -0300

    Componente Modal

--- makeup/src/App.js
+++ makeup/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Products from './Components/Products/Products';
+import Modal from './Components/Modal/Modal';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Products />
+        <Products/>
+        <Modal/>
       </header>
     </div>
   );
--- makeup/src/Components/Products/styles.js
+++ makeup/src/Components/Products/styles.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
+
 export const Container = styled.div`
-  display: flex;
+  display: flex; 
   flex-direction: column;
   height: 100vh;
   width: min-content;
+  color: rgb(32, 32, 32);
+  margin-inline: 1%;
 `;
 
 export const StyledImage = styled.img`
@@ -15,10 +18,12 @@ export const StyledImage = styled.img`
 export const Ul = styled.ul `
   list-style-type: none;
   padding: 0;
+  flex-direction: row; 
 `;
 
 export const Li = styled.li `
-  display: flex;
-  align-items: center;
-  flex-direction: column;
+  margin-bottom: 10%;
+  width: 100%;
+  background-color: white;
+  border-radius: 5%;
 `;
